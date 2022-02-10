2ff9818230516294c6f7308d9e2098bcc934dfe8
mashiiii <mahvishr20@gmail.com>
2022-02-10 12:40:37 +0000

    file

--- front/hardhat.config.js
+++ front/hardhat.config.js
@@ -1,21 +1,31 @@
-require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-web3";
 
-  for (const account of accounts) {
-    console.log(account.address);
+require('dotenv').config()
+const fs = require('fs');
+const privateKey = fs.readFileSync(".secret").toString().trim() || "8ce898b3863037edaba4834d922d68200820f198ec6977352ddb59dea4379b1d";
+
+export default {
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    kovan: {
+      url: "https://kovan.infura.io/v3/8c661edd6d764e1e95fd0318054d331c",
+      accounts: [privateKey]
+
+    },
+	bsc:{
+		url: "https://bsc-dataseed.binance.org/",
+		accounts: [privateKey]
+	}
   }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.4",
-};
+}
